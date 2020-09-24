d34b1ce36c8026e4c8e11fff0c8a16e8ec12e7fb
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-09-24 17:20:15 -0600

    added a generation counter, added functionality to disable users ability to click a cell to life while simulation is running, added buttons for multiple grid size options, added slider with Material UI for different speed options.

---
+++ game-of-life/src/components/Life.js
@@ -0,0 +1,218 @@
+import React, { useState, useCallback, useRef } from "react";
+// import "../styles";
+import produce from "immer";
+
+import Slider from "@material-ui/core/Slider";
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+]; // represents locations surrounding current cell (neighbor cells)
+
+function Life() {
+  const [generations, setGenerations] = useState(0);
+  const [playSpeed, setPlaySpeed] = useState(100); // default 100 ms
+  const [gridSize, setGridSize] = useState(42); // initial grid size is 42x42
+
+  const numRows = gridSize;
+  const numCols = gridSize;
+
+  const generateEmptyGrid = () => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+  }; // clears the grid
+
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid();
+  });
+
+  const resizeGrid = size => {
+    setGridSize(size);
+    setGrid(generateEmptyGrid());
+  };
+
+  const [running, setRunning] = useState(false); // by default 'running' state is false
+
+  const runningRef = useRef(running); // useRef hook imported from React
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return; // recursive, whenever not running: kill simulation
+    }
+
+    // SIMULATE. uses double for loops to run thru every value in the grid (rows & cols)
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK]; // tells us how many neighbors the current cell has
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+              // Rule 1, Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+              // Rule 3, Any live cell with more than three live neighbours dies, as if by overpopulation.
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+              // Rule 4, Any dead cell with EXACTLY THREE live neighbours becomes a live cell, as if by reproduction.
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(() => {
+      runSimulation();
+      setGenerations(gen => gen + 1);
+      console.log(generations);
+    }, playSpeed);
+  }, [numCols, numRows]);
+
+  const handleChange = (e, newVal) => {
+    setPlaySpeed(newVal);
+  };
+
+  //   const manualStep = () => {
+
+  //     setGenerations((gen) => gen + 1);
+  //   }
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+        }}
+      >
+        {running ? "STOP LIFE" : "START LIFE"}
+      </button>
+      <button>STEP</button>
+      <button
+        onClick={() => {
+          setGrid(generateEmptyGrid());
+          setGenerations(0);
+        }}
+      >
+        CLEAR
+      </button>
+      <button
+        onClick={() => {
+          const rows = [];
+          for (let i = 0; i < numRows; i++) {
+            rows.push(
+              Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
+            );
+          }
+
+          setGrid(rows);
+          setGenerations(0);
+        }}
+      >
+        RANDOMIZE
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          resizeGrid(25);
+        }}
+      >
+        {running ? "!25x25!" : "25x25"}
+      </button>
+      <button
+        onClick={() => {
+          resizeGrid(35);
+        }}
+      >
+        {running ? "!35x35!" : "35x35"}
+      </button>
+      <button
+        onClick={() => {
+          resizeGrid(50);
+        }}
+      >
+        {running ? "!50x50!" : "50x50"}
+      </button>
+      <button
+        onClick={() => {
+          resizeGrid(42);
+        }}
+      >
+        {running ? "!42x42!" : "42x42"}
+      </button>
+      <h2>Generation counter: {generations}</h2>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((cols, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1; // setting initial state for grid
+                  // immer library makes this an immutable change and generate a new grid
+                });
+                setGrid(newGrid);
+              }}
+              style={
+                running // make div unclickable while simulation is running
+                  ? {
+                      pointerEvents: "none",
+                      width: 20,
+                      height: 20,
+                      backgroundColor: grid[i][k] ? "#EC87E4" : "#D3FBD8", // ternary to decide color of live/dead cells
+                      border: "solid 1px #000000",
+                    }
+                  : {
+                      width: 20,
+                      height: 20,
+                      backgroundColor: grid[i][k] ? "red" : "#D3FBD8", // ternary to decide color of live/dead cells
+                      border: "solid 1px #000000",
+                    }
+              }
+            />
+          ))
+        )}
+      </div>
+      <h2>
+        Speed Slider (ms)
+        <Slider
+          label="speed"
+          value={playSpeed}
+          onChange={handleChange}
+          min={100}
+          max={1000}
+          step={100}
+          valueLabelDisplay="auto"
+          marks={true}
+        />
+      </h2>
+    </div>
+  );
+}
+
+export default Life;
